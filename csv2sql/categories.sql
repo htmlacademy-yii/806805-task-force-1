30376f1f37d7c11acab5e0a392ea7c7bfcf325c3
allomir <49421075+allomir@users.noreply.github.com>
2019-12-17 20:40:02 +0300

    #5 fire best of the best

---
+++ csv2sql/categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (name, symbol)
+                 VALUES ('Курьерские услуги', 'translation');INSERT INTO categories (name, symbol)
+                 VALUES ('Уборка', 'clean');INSERT INTO categories (name, symbol)
+                 VALUES ('Переезды', 'cargo');INSERT INTO categories (name, symbol)
+                 VALUES ('Компьютерная помощь', 'neo');INSERT INTO categories (name, symbol)
+                 VALUES ('Ремонт квартирный', 'flat');INSERT INTO categories (name, symbol)
+                 VALUES ('Ремонт техники', 'repair');INSERT INTO categories (name, symbol)
+                 VALUES ('Красота', 'beauty');INSERT INTO categories (name, symbol)
+                 VALUES ('Фото', 'photo');
