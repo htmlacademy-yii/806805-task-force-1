d3646c2dd895c12246a3bdc57fd79df56c07518b
allomir <49421075+allomir@users.noreply.github.com>
2020-01-17 14:28:33 +0300

    # schema data dump export

--- schemas/dump.sql
+++ schemas/dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `task_force` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `task_force`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: task_force
@@ -544,4 +546,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-17 14:00:11
+-- Dump completed on 2020-01-17 14:24:37
