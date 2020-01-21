3f64f211d70fb9b80ea119e60483fd76c8f79d3d
allomir <alomir@yandex.ru>
2020-01-21 18:05:57 +0300

    # myExample

--- frontend/controllers/AnyTestsController.php
+++ frontend/controllers/AnyTestsController.php
@@ -3,12 +3,14 @@
 namespace frontend\controllers;
 
 use yii\web\Controller;
+use yii;
+use yii\db\Connection;
 use yii\db\Query;
 use frontend\models\db\Tasks;
 use yii\web\NotFoundHttpException;
 
 
-class ExamplesController extends Controller
+class AnytestsController extends Controller
 {
 
     public function d($value) {
@@ -19,6 +21,31 @@ class ExamplesController extends Controller
 
     public function actionIndex() 
     {
+        $pageSets = ['title' => 'hellow world'];
+        return $this->render('index', ['pageSets' => $pageSets]);
+    }
+
+    public function actionConn() {
+        
+        // Подключение к базе данных вручную
+        $db = new yii\db\Connection([
+            'dsn' => 'mysql:host=localhost;dbname=task_force',
+            'username' => 'user',
+            'password' => 'Universal85',
+            'charset' => 'utf8',
+        ]);
+
+        // $table = Yii::$app->db->getTableSchema('categories'); // выполнение запросов с помощью подключения глобально из настроек приложения и его компонентов
+        $table = $db->getTableSchema('categories'); // выполнение запросов с использованием подключения вручную, те заданного не глобально а частно.
+
+        $this->d($table);
+
+        $pageSets = ['title' => 'Подключение к БД'];
+        return $this->render('conn', ['pageSets' => $pageSets]);
+    }
+
+    public function actionTasks() {
+
         date_default_timezone_set('Europe/Moscow');
         /* Пример 1 */
         // Пользователи являются Исполнителями, если они не являются заказчиками в текущий момент, те когда Task_status=new и Task_status=running
@@ -70,40 +97,21 @@ class ExamplesController extends Controller
         // $task = Tasks::find()->where(['id_task' => 3])->joinWith('category')->limit(1)->one();
         // $task = Tasks::find()->where(['status_id' => 3])->joinWith('category')->limit(1)->one();
 
-        return $this->render('index', ['tasks' => $tasks]);
-
-    }
- 
-    public function actionArr($id) {
 
         // Примеры - данные как массивы
         // $tasks = Tasks::find()->where(['category_id' => 3])->orderBy('id_task')->joinWith('category')->limit(3)->asArray()->all();
         
-        // печать примеров как массив
-        foreach($tasks as $task) {
-            print_r($task); // как массивы
-        }
-        print('<hr>');
-        $task = Tasks::findOne($id);
-
         print('<pre>');
-
-        print_r($task);
-        print('<hr>');
-
-        // print_r ($task->category);
-        print('<hr>');
-        // print_r ($task->location);
-        print('<hr>');
-        // print_r ($task->status);
-        print('<hr>');
-        //print_r ($task->customer);
+        // печать примеров как массив
+        // print('<hr>');
+        // $task = Tasks::findOne($id);
 
         // @property TaskRunnings[] $taskRunnings
         // print_r ($task->taskRunnings[0]->task_running_id);
         print('</pre>');
 
-        return $this->render('view', ['task' => $task]);
+        $pageSets = ['title' => 'Подключение к БД'];
+        return $this->render('tasks', ['tasks' => $tasks, 'pageSets' => $pageSets]);
 
     }
 
