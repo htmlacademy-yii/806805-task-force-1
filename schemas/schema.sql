c859136d5c7a5d9cd05f125b671a047cc7831981
allomir <alomir@yandex.ru>
2020-07-01 07:03:59 +0300

    Оформление кода. Схема БД

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -7,8 +7,8 @@ USE task_force;
 
 
 CREATE TABLE IF NOT EXISTS locations (
-    PRIMARY KEY (location_id),               -- id_location -> location_id
-    location_id INT AUTO_INCREMENT NOT NULL, -- id_location -> location_id
+    PRIMARY KEY (location_id),               
+    location_id INT AUTO_INCREMENT NOT NULL, 
     city        VARCHAR(64)        NOT NULL, 
     latitude    VARCHAR(255)       NOT NULL,
     longitude   VARCHAR(255)       NOT NULL
@@ -16,65 +16,65 @@ CREATE TABLE IF NOT EXISTS locations (
 
 
 CREATE TABLE IF NOT EXISTS task_statuses (
-    PRIMARY KEY (status_id),                      -- id_task_status -> status_id
-    status_id  INT AUTO_INCREMENT NOT NULL,        -- id_task_status -> status_id
-    title      VARCHAR(64)        NOT NULL UNIQUE, -- name -> title
-    const_name VARCHAR(64)        NOT NULL UNIQUE  -- symbol -> const_name
+    PRIMARY KEY (status_id),                      
+    status_id  INT AUTO_INCREMENT NOT NULL,        
+    title      VARCHAR(64)        NOT NULL UNIQUE, 
+    const_name VARCHAR(64)        NOT NULL UNIQUE  
 );
 
 
 CREATE TABLE IF NOT EXISTS task_actions (
-    PRIMARY KEY (action_id),                                -- id_task_action -> action_id
-    action_id  INT AUTO_INCREMENT NOT NULL,    -- id_task_action -> action_id
-    title      VARCHAR(64)        NOT NULL UNIQUE,                 -- name -> title
-    const_name VARCHAR(64)        NOT NULL UNIQUE   -- symbol -> const_name
+    PRIMARY KEY (action_id),                                
+    action_id  INT AUTO_INCREMENT NOT NULL,    
+    title      VARCHAR(64)        NOT NULL UNIQUE,                 
+    const_name VARCHAR(64)        NOT NULL UNIQUE   
 );
 
 
 CREATE TABLE IF NOT EXISTS user_roles (
-    PRIMARY KEY (role_id),                                 -- id_user_role -> role_id
-    role_id    INT AUTO_INCREMENT NOT NULL,  -- id_user_role -> role_id
-    title      VARCHAR(64)        NOT NULL UNIQUE, -- name -> title
-    const_name VARCHAR(64)        NOT NULL UNIQUE -- symbol -> const_name
+    PRIMARY KEY (role_id),                                 
+    role_id    INT AUTO_INCREMENT NOT NULL,  
+    title      VARCHAR(64)        NOT NULL UNIQUE, 
+    const_name VARCHAR(64)        NOT NULL UNIQUE 
 );
 
 
 CREATE TABLE IF NOT EXISTS categories (
-    PRIMARY KEY (category_id),   -- id_category -> category_id
-    category_id INT AUTO_INCREMENT NOT NULL, -- id_category -> category_id
-    title       VARCHAR(64)        NOT NULL UNIQUE, -- name -> title
-    label       VARCHAR(64)        NOT NULL UNIQUE   -- symbol -> label
+    PRIMARY KEY (category_id),   
+    category_id INT AUTO_INCREMENT NOT NULL, 
+    title       VARCHAR(64)        NOT NULL UNIQUE, 
+    label       VARCHAR(64)        NOT NULL UNIQUE   
 );
 
 
 CREATE TABLE IF NOT EXISTS users (
-    PRIMARY KEY (user_id),                      -- id_user -> user_id
-    user_id           INT AUTO_INCREMENT NOT NULL, -- id_user -> user_id
+    PRIMARY KEY (user_id),                      
+    user_id           INT AUTO_INCREMENT NOT NULL, 
 	role_id           INT DEFAULT 1      NOT NULL, 
 	location_id       INT                NOT NULL,
-    full_name         VARCHAR(64)       NOT NULL, -- name -> full_name
+    full_name         VARCHAR(64)       NOT NULL, 
 	email             VARCHAR(64)       NOT NULL UNIQUE,
     phone             VARCHAR(11),
     skype             VARCHAR(64),
-	messaging_contact VARCHAR(64),           -- other_contacts -> messaging_contact
-    full_address      VARCHAR(255),                -- address -> full_address
-    avatar_addr       VARCHAR(255),                -- avatar -> avatar_addr
-	desc_text         TEXT,                        -- about -> self_desc
+	messaging_contact VARCHAR(64),           
+    full_address      VARCHAR(255),                
+    avatar_addr       VARCHAR(255),                
+	desc_text         TEXT,                        
     password_key      VARCHAR(255)       NOT NULL,
 	birth_date        DATE,
     reg_time          DATETIME           NOT NULL,
 	activity_time     DATETIME           NOT NULL,
     hide_contacts     BOOL DEFAULT 0     NOT NULL,
     hide_profile      BOOL DEFAULT 0     NOT NULL,
-    FOREIGN KEY (role_id)     REFERENCES user_roles(role_id),        -- id_user_role -> role_id
-    FOREIGN KEY (location_id) REFERENCES locations(location_id),  -- id_location -> location_id
+    FOREIGN KEY (role_id)     REFERENCES user_roles(role_id),       
+    FOREIGN KEY (location_id) REFERENCES locations(location_id),  
     FULLTEXT users_full_name_fulltext (full_name)
 );
 
 
 CREATE TABLE IF NOT EXISTS user_portfolio_images (
     PRIMARY KEY (image_id),
-    image_id   INT AUTO_INCREMENT NOT NULL, -- id_user_portfolio_image -> image_id
+    image_id   INT AUTO_INCREMENT NOT NULL, 
     user_id    INT                NOT NULL,
     image_addr VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
@@ -82,8 +82,8 @@ CREATE TABLE IF NOT EXISTS user_portfolio_images (
 
 
 CREATE TABLE IF NOT EXISTS user_specializations (
-    PRIMARY KEY (specialization_id),            -- id_user_specialization -> specialization_id
-	specialization_id INT AUTO_INCREMENT NOT NULL, -- id_user_specialization -> specialization_id
+    PRIMARY KEY (specialization_id),            
+	specialization_id INT AUTO_INCREMENT NOT NULL, 
 	user_id           INT                NOT NULL,
 	category_id       INT,
     FOREIGN KEY (user_id)     REFERENCES users(user_id),
@@ -93,7 +93,7 @@ CREATE TABLE IF NOT EXISTS user_specializations (
 
 CREATE TABLE IF NOT EXISTS user_notifications (
     PRIMARY KEY (notification_id),
-    notification_id INT AUTO_INCREMENT NOT NULL, -- id_user_notification -> notification_id
+    notification_id INT AUTO_INCREMENT NOT NULL, 
     title           VARCHAR(64)        NOT NULL UNIQUE,
 	label           VARCHAR(64)        NOT NULL UNIQUE
 );
@@ -101,27 +101,27 @@ CREATE TABLE IF NOT EXISTS user_notifications (
 
 CREATE TABLE IF NOT EXISTS user_notification_settings (
     PRIMARY KEY (setting_id),
-    setting_id      INT AUTO_INCREMENT NOT NULL, -- id_user_notification_setting -> setting_id
+    setting_id      INT AUTO_INCREMENT NOT NULL, 
 	user_id         INT                NOT NULL,
     notification_id INT                NOT NULL,
-    is_active       BOOL DEFAULT 1,                 -- on_off -> is_active
+    is_active       BOOL DEFAULT 1,                 
     FOREIGN KEY (user_id)         REFERENCES users(user_id),
     FOREIGN KEY (notification_id) REFERENCES user_notifications(notification_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS tasks (
-    PRIMARY KEY (task_id),                      -- id_task -> task_id
-    task_id      INT AUTO_INCREMENT NOT NULL,    -- id_task -> task_id
+    PRIMARY KEY (task_id),                      
+    task_id      INT AUTO_INCREMENT NOT NULL,    
 	status_id    INT                NOT NULL,
     category_id  INT                NOT NULL,
 	location_id  INT                NOT NULL,
     customer_id  INT                NOT NULL,
     title        VARCHAR(128)       NOT NULL,
-    desc_text    TEXT               NOT NULL,          -- description -> desc_text
+    desc_text    TEXT               NOT NULL,          
     price        INT UNSIGNED,
     full_address VARCHAR(255),
-    address_desc VARCHAR(255),      -- новый
+    address_desc VARCHAR(255),      
     latitude     VARCHAR(255),
     longitude    VARCHAR(255),
     add_time     DATETIME           NOT NULL,
@@ -136,8 +136,8 @@ CREATE TABLE IF NOT EXISTS tasks (
 
 
 CREATE TABLE IF NOT EXISTS task_files (
-    PRIMARY KEY (file_id),          -- id_task_file -> file_id
-    file_id   INT AUTO_INCREMENT NOT NULL,  -- id_task_file -> file_id
+    PRIMARY KEY (file_id),          
+    file_id   INT AUTO_INCREMENT NOT NULL,  
     task_id   INT                NOT NULL,
     file_addr VARCHAR(255),
     FOREIGN KEY (task_id) REFERENCES tasks(task_id)
@@ -145,9 +145,9 @@ CREATE TABLE IF NOT EXISTS task_files (
 
 
 CREATE TABLE IF NOT EXISTS task_runnings (
-    PRIMARY KEY (running_id),           -- id_task_running -> running_id
-    running_id    INT AUTO_INCREMENT NOT NULL,  -- id_task_running -> running_id
-    task_id       INT                NOT NULL,      -- task_running_id -> task_id
+    PRIMARY KEY (running_id),           
+    running_id    INT AUTO_INCREMENT NOT NULL,  
+    task_id       INT                NOT NULL,      
     contractor_id INT                NOT NULL,
     FOREIGN KEY (task_id)       REFERENCES tasks(task_id),
     FOREIGN KEY (contractor_id) REFERENCES users(user_id)
@@ -155,8 +155,8 @@ CREATE TABLE IF NOT EXISTS task_runnings (
 
 
 CREATE TABLE IF NOT EXISTS task_failings (
-    PRIMARY KEY (failing_id),  -- id_task_failing -> failing_id
-    failing_id    INT AUTO_INCREMENT NOT NULL, -- id_task_failing -> failing_id
+    PRIMARY KEY (failing_id),  
+    failing_id    INT AUTO_INCREMENT NOT NULL,
     task_id       INT                NOT NULL,
     contractor_id INT                NOT NULL,
     FOREIGN KEY (task_id)       REFERENCES tasks(task_id),
@@ -165,13 +165,13 @@ CREATE TABLE IF NOT EXISTS task_failings (
 
 
 CREATE TABLE IF NOT EXISTS feedbacks (
-    PRIMARY KEY (feedback_id),  -- id_feedback -> feedback_id
-    feedback_id  INT AUTO_INCREMENT NOT NULL,  -- id_feedback -> feedback_id
-    author_id    INT                NOT NULL,     -- user_id -> author_id
-    recipient_id INT                NOT NULL,    -- user_rated_id -> recipient_id
+    PRIMARY KEY (feedback_id),  
+    feedback_id  INT AUTO_INCREMENT NOT NULL,  
+    author_id    INT                NOT NULL,     
+    recipient_id INT                NOT NULL,    
     task_id      INT                NOT NULL,
-    desc_text    TEXT,        -- desk -> desc_text
-    point_num    INT                NOT NULL,  -- point -> point_num
+    desc_text    TEXT,        
+    point_num    INT                NOT NULL,  
     add_time     DATETIME           NOT NULL,
     FOREIGN KEY (author_id)    REFERENCES users(user_id),
     FOREIGN KEY (recipient_id) REFERENCES users(user_id),
@@ -180,33 +180,33 @@ CREATE TABLE IF NOT EXISTS feedbacks (
 
 
 CREATE TABLE IF NOT EXISTS offers (
-    PRIMARY KEY (offer_id),     -- id_offer -> offer_id
-    offer_id      INT AUTO_INCREMENT NOT NULL,    -- id_offer -> offer_id
+    PRIMARY KEY (offer_id),    
+    offer_id      INT AUTO_INCREMENT NOT NULL,    
     task_id       INT                NOT NULL,
     contractor_id INT                NOT NULL,
-    desc_text     TEXT               NOT NULL,        -- desk -> desc_text
+    desc_text     TEXT               NOT NULL,        
     FOREIGN KEY (task_id)       REFERENCES tasks(task_id),
     FOREIGN KEY (contractor_id) REFERENCES users(user_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS user_favorites (
-    PRIMARY KEY (favorite_id),     -- id_user_favorite -> favorite_id
-    favorite_id  INT AUTO_INCREMENT NOT NULL, -- id_user_favorite -> favorite_id
+    PRIMARY KEY (favorite_id),     
+    favorite_id  INT AUTO_INCREMENT NOT NULL, 
     user_id      INT                NOT NULL,
     fave_user_id INT                NOT NULL,
-    is_fave      BOOL DEFAULT 1,                 -- on_off -> is_fave
+    is_fave      BOOL DEFAULT 1,                 
     FOREIGN KEY (user_id)      REFERENCES users(user_id),
     FOREIGN KEY (fave_user_id) REFERENCES users(user_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS messages (
-    PRIMARY KEY (message_id),       -- id_message -> message_id
+    PRIMARY KEY (message_id),       
     message_id  INT AUTO_INCREMENT NOT NULL,
     task_id     INT                NOT NULL,
     sender_id   INT                NOT NULL,
-    receiver_id INT                NOT NULL,         -- recipient_id -> receiver_id
+    receiver_id INT                NOT NULL,         
     mess_text   TEXT               NOT NULL,
     add_time    DATETIME           NOT NULL,
     FOREIGN KEY (task_id)     REFERENCES tasks(task_id),
