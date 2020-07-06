9584d68e89f72ad26b6575d2ee6425f531a7e011
allomir <alomir@yandex.ru>
2020-07-06 05:33:47 +0300

    Задание. Страница task/view/id

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -2,8 +2,10 @@
 
 namespace frontend\controllers;
 
+use frontend\models\db\Tasks;
 use frontend\models\forms\TasksFilters;
 use frontend\models\forms\TasksForm;
+use frontend\models\forms\UsersFilters;
 use yii;
 use yii\web\Controller;
 
@@ -21,13 +23,63 @@ class TasksController extends Controller
             $tasks = $tasksFilters->getNewTasks($tasksForm);
         }
 
-        return $this->render('index', ['tasks' => $tasks, 'tasksForm' => $tasksForm]);
+        return $this->render('index', [
+            'tasks' => $tasks,
+            'tasksForm' => $tasksForm,
+        ]);
     }
 
     public function actionView(int $id = null)
     {
+        $taskQuery = Tasks::find()
+            ->joinWith([
+                'status',
+                'category',
+                'taskFiles',
+                'location',
+                'offers.contractor oc',
+            ])
+            ->where($id)
+            ->limit(1);
+        $task = $taskQuery->one();
 
-        
-        return $this->render('view', []);
+        define('USER_ID', 11); // условно текущий пользователь, влияет на показ мини-пользователь и чат
+
+        $customer = $task->customer->attributes;
+        $customerRating = UsersFilters::getRatingGeneric(
+            $task['customer_id']) ?: [];
+        $customer = array_merge($customer, $customerRating);
+        $customer['tasks'] = $task->customer->customerTasks;
+
+        $contractor = null;
+        if ($taskQuery->andWhere(['tasks.status_id' => 3])->exists()) {
+            $contractor = $task->taskRunnings->contractor->attributes;
+            $contractorRating = UsersFilters::getRatingGeneric(
+                $contractor['user_id']) ?: [];
+            $contractor = array_merge($contractor, $contractorRating);
+            $contractor['tasks'] = UsersFilters::getContractorTasks(
+                $contractor['user_id']
+            );
+        }
+
+        $offerContractors = null;
+        if ($task->offers !== null) {
+            $contractorRaitings = UsersFilters::getRatingGeneric(
+                array_column($task->offers, 'contractor_id'));
+
+            foreach ($task->offers as $offer) {
+                $offerContractor = $offer->contractor->attributes;
+                $contractorRaiting = $contractorRaitings[$offerContractor['user_id']];
+                $offerContractors[$offer['offer_id']] = array_merge(
+                    $offerContractor, $contractorRaiting);
+            }
+        }
+
+        return $this->render('view', [
+            'task' => $task,
+            'customer' => $customer,
+            'contractor' => $contractor,
+            'offerContractors' => $offerContractors,
+        ]);
     }
 }
--- frontend/views/tasks/view.php
+++ frontend/views/tasks/view.php
@@ -1,5 +1,4 @@
 <?php
-use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 // use yii\widgets\ActiveField; // Не используем
 
@@ -10,47 +9,52 @@ $this->title = 'Просмотр задания (view.html)';
 
 <section class="content-view">
     <div class="content-view__card">
+
+        <!-- задание -->
         <div class="content-view__card-wrapper">
             <div class="content-view__header">
                 <div class="content-view__headline">
-                    <h1>Убрать квартиру после вписки</h1>
+                    <h1><?=$task->title?></h1>
                     <span>Размещено в категории
-                        <a href="#" class="link-regular">Уборка</a>
+                        <a href="#" class="link-regular"><?=$task->category['title']?></a>
                         25 минут назад</span>
                 </div>
-                <b class="new-task__price new-task__price--clean content-view-price">1500<b> ₽</b></b>
-                <div class="new-task__icon new-task__icon--clean content-view-icon"></div>
+                <b class="new-task__price new-task__price--clean content-view-price"><?=$task->price?><b> ₽</b></b>
+                <div class="new-task__icon new-task__icon--<?=$task->category['label']?> content-view-icon"></div>
             </div>
             <div class="content-view__description">
                 <h3 class="content-view__h3">Общее описание</h3>
                 <p>
-                    Внезапно, ключевые особенности структуры проекта неоднозначны и будут подвергнуты целой серии
-                    независимых исследований. Следует отметить, что высококачественный прототип будущего проекта, в
-                    своём классическом представлении, допускает внедрение своевременного выполнения сверхзадачи.
-                    Кстати, некоторые особенности внутренней политики будут функционально разнесены на независимые
-                    элементы.
+                <?=$task->desc_text?>
                 </p>
             </div>
             <div class="content-view__attach">
+                <!-- файлы итерация-->
                 <h3 class="content-view__h3">Вложения</h3>
-                <a href="#">my_picture.jpeg</a>
-                <a href="#">agreement.docx</a>
+                <?php foreach ($task->taskFiles as $file): ?>
+                <a href="<?=$file->file_addr?>"><?=$file->file_addr?></a>
+                <?php endforeach;?>
+                <!-- /файлы итерация-->
             </div>
+            <!-- адрес -->
             <div class="content-view__location">
                 <h3 class="content-view__h3">Расположение</h3>
                 <div class="content-view__location-wrapper">
                     <div class="content-view__map">
-                        <a href="#"><img src="./img/map.jpg" width="361" height="292"
+                        <a href="#"><img src="/img/map.jpg" width="361" height="292"
                                             alt="Москва, Новый арбат, 23 к. 1"></a>
                     </div>
                     <div class="content-view__address">
-                        <span class="address__town">Москва</span><br>
-                        <span>Новый арбат, 23 к. 1</span>
-                        <p>Вход под арку, код домофона 1122</p>
+                        <span class="address__town"><?=$task->location['city']?></span><br>
+                        <span><?=$task->full_address?></span>
+                        <p><?=$task->address_desc?></p>
                     </div>
                 </div>
             </div>
+            <!-- /адрес -->
         </div>
+        <!-- /задание -->
+
         <div class="content-view__action-buttons">
                 <button class=" button button__big-color response-button open-modal"
                         type="button" data-for="response-form">Откликнуться</button>
@@ -59,25 +63,45 @@ $this->title = 'Просмотр задания (view.html)';
             <button class="button button__big-color request-button open-modal"
                     type="button" data-for="complete-form">Завершить</button>
         </div>
+
     </div>
     <div class="content-view__feedback">
-        <h2>Отклики <span>(2)</span></h2>
+        <h2>Отклики <span><?=count($task->offers)?></span></h2>
         <div class="content-view__feedback-wrapper">
+
+            <!-- Отклики (предложения) итерация -->
+            <?php foreach ($task->offers as $offer): 
+                $offerContractor = $offerContractors[$offer->offer_id];
+            ?>
             <div class="content-view__feedback-card">
                 <div class="feedback-card__top">
-                    <a href="#"><img src="./img/man-glasses.jpg" width="55" height="55"></a>
+                    <a href="#"><img src="/<?=$offerContractor['avatar_addr'] ?: \Yii::$app->params['defaultAvatarAddr']?>" width="55" height="55"></a>
                     <div class="feedback-card__top--name">
-                        <p><a href="#" class="link-regular">Астахов Павел</a></p>
-                        <span></span><span></span><span></span><span></span><span class="star-disabled"></span>
-                        <b>4.25</b>
+                        <p><a href="#" class="link-regular"><?=$offerContractor['full_name']?></a></p>
+                        <!-- Рейтинг -->
+                        <?php $avg_point = $offerContractor['avg_point'] ?? 0;?>
+                        
+                        <!-- итерация желтой звездочки -->
+                        <?php for ($i = 1; $i <= $avg_point; $i++): ?>
+                            <span></span>
+                        <?php endfor;?>
+
+                        <!-- итерация белой звездочки -->
+                        <?php for ($i = $avg_point; $i < 5; $i++): ?>
+                            <span class="star-disabled"></span>
+                        <?php endfor;?>
+                        
+                        <!-- Средний балл -->
+                        <b><?=substr($avg_point, 0, 4)?></b>
+                        <!-- /Рейтинг -->
                     </div>
                     <span class="new-task__time">25 минут назад</span>
                 </div>
                 <div class="feedback-card__content">
                     <p>
-                        Могу сделать всё в лучшем виде. У меня есть необходимый опыт и инструменты.
+                        <?=$offer->desc_text?>
                     </p>
-                    <span>1500 ₽</span>
+                    <span><?=$offer->price?> ₽</span>
                 </div>
                 <div class="feedback-card__actions">
                     <a class="button__small-color request-button button"
@@ -86,29 +110,9 @@ $this->title = 'Просмотр задания (view.html)';
                             type="button">Отказать</a>
                 </div>
             </div>
-            <div class="content-view__feedback-card">
-                <div class="feedback-card__top">
-                    <a href="#"><img src="./img/man-blond.jpg" width="55" height="55"></a>
-                    <div class="feedback-card__top--name">
-                        <p class="link-name"><a href="#" class="link-regular">Богатырев Дмитрий</a></p>
-                        <span></span><span></span><span></span><span></span><span class="star-disabled"></span>
-                        <b>4.25</b>
-                    </div>
-                    <span class="new-task__time">25 минут назад</span>
-                </div>
-                <div class="feedback-card__content">
-                    <p>
-                        Примусь за выполнение задания в течение часа, сделаю быстро и качественно.
-                    </p>
-                    <span>1500 ₽</span>
-                </div>
-                <div class="feedback-card__actions">
-                    <a class="button__small-color request-button button"
-                            type="button">Подтвердить</a>
-                    <a class="button__small-color refusal-button button"
-                            type="button">Отказать</a>
-                </div>
-            </div>
+            <?php endforeach;?>
+            <!-- /Отклики (предложения) итерация -->
+
         </div>
     </div>
 </section>
@@ -120,18 +124,55 @@ $this->title = 'Просмотр задания (view.html)';
 
 <section class="connect-desk">
     <div class="connect-desk__profile-mini">
+        <?php if ($contractor === null OR USER_ID !== $task['customer_id']): ?>
+        <!-- мини профиль заказчика -->
         <div class="profile-mini__wrapper">
-            <h3>Заказчик</h3>
+            <h3>Заказчик (<?="id{$task['customer_id']}"?>)</h3>
             <div class="profile-mini__top">
-                <img src="./img/man-brune.jpg" width="62" height="62" alt="Аватар заказчика">
+                <img src="/<?=$customer['avatar_addr'] ?: \Yii::$app->params['defaultAvatarAddr']?>" width="62" height="62" alt="Аватар заказчика">
                 <div class="profile-mini__name five-stars__rate">
-                    <p>Николай Демченко</p>
+                    <p><?=$customer['full_name']?></p>
                 </div>
             </div>
-            <p class="info-customer"><span>12 заданий</span><span class="last-">2 года на сайте</span></p>
+            <p class="info-customer"><span><?=count($customer['tasks'])?> заданий</span><span class="last-">2 года на сайте</span></p>
             <a href="#" class="link-regular">Смотреть профиль</a>
         </div>
+        <!-- /мини профиль заказчика -->
+        <?php else: ?>
+        <!-- мини профиль исполнителя -->
+        <div class="profile-mini__wrapper">
+            <h3>Исполнитель (<?="id{$contractor['user_id']}"?>)</h3>
+            <div class="profile-mini__top">
+                <img src="/<?=$contractor['avatar_addr'] ?: \Yii::$app->params['defaultAvatarAddr']?>" width="62" height="62" alt="Аватар заказчика">
+                <div class="profile-mini__name five-stars__rate">
+                    <p><?=$contractor['full_name']?></p>
+                    <!-- Рейтинг -->
+                    <?php $avg_point = $contractor['avg_point'] ?? 0;?>
+                    
+                    <!-- итерация желтой звездочки -->
+                    <?php for ($i = 1; $i <= $avg_point; $i++): ?>
+                        <span></span>
+                    <?php endfor;?>
+
+                    <!-- итерация белой звездочки -->
+                    <?php for ($i = $avg_point; $i < 5; $i++): ?>
+                        <span class="star-disabled"></span>
+                    <?php endfor;?>
+                    
+                    <!-- Средний балл -->
+                    <b><?=substr($avg_point, 0, 4)?></b>
+                    <!-- /Рейтинг -->
+                </div>
+            </div>
+            <p class="info-customer"><span><?=count($contractor->tasks)?> заданий</span><span class="last-">2 года на сайте</span></p>
+            <a href="#" class="link-regular">Смотреть профиль</a>
+        </div>
+        <!-- /мини профиль исполнителя -->
+        <?php endif; ?>
     </div>
+
+    <!-- Чат -->
+    <?php if ($contractor === null && (USER_ID === $contractor['user_id'] || USER_ID === $task['customer_id'])): ?>
     <div class="connect-desk__chat">
         <h3>Переписка</h3>
         <div class="chat__overflow">
@@ -156,6 +197,8 @@ $this->title = 'Просмотр задания (view.html)';
             <button class="button chat__button" type="submit">Отправить</button>
         </form>
     </div>
+    <?php endif; ?>
+    <!-- /чат -->
 </section>
 
 <!-- /контент view.html правая секция -->
