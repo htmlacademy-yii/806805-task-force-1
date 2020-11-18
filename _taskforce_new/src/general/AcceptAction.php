2b391ae8644507f36a5e8cde7d6c682e81f50aee
allomir <alomir@yandex.ru>
2020-11-18 07:27:13 +0300

    правки оформление

--- _taskforce_new/src/general/AcceptAction.php
+++ _taskforce_new/src/general/AcceptAction.php
@@ -9,7 +9,7 @@ class AcceptAction extends AbstractAction
         return 'action_accept';
     }
 
-    public static function verifyAccess(AvailableActions $availableActions, int $userId): bool
+    public static function verifyAccess(AvailableActions $availableActions, $userId): bool
     {
         if ($availableActions->getCurrentStatus() === $availableActions::STATUS_RUNNING 
             && $availableActions->checkRoleInTask($userId) === $availableActions::ROLE_CONTRACTOR) {
