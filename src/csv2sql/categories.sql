785c6af5482d95512ecc14eb07930445c70429c4
allomir <alomir@yandex.ru>
2021-03-09 17:39:03 +0300

    чистка

--- src/csv2sql/categories.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO categories (name, symbol)
-                 VALUES ('Курьерские услуги', 'translation');INSERT INTO categories (name, symbol)
-                 VALUES ('Уборка', 'clean');INSERT INTO categories (name, symbol)
-                 VALUES ('Переезды', 'cargo');INSERT INTO categories (name, symbol)
-                 VALUES ('Компьютерная помощь', 'neo');INSERT INTO categories (name, symbol)
-                 VALUES ('Ремонт квартирный', 'flat');INSERT INTO categories (name, symbol)
-                 VALUES ('Ремонт техники', 'repair');INSERT INTO categories (name, symbol)
-                 VALUES ('Красота', 'beauty');INSERT INTO categories (name, symbol)
-                 VALUES ('Фото', 'photo');
--- src/csv2sql/user_notifications.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user_notifications (symbol, name)
-                 VALUES ('notice_mess', 'Новое сообщение');INSERT INTO user_notifications (symbol, name)
-                 VALUES ('notice_action', 'Действия по заданию');INSERT INTO user_notifications (symbol, name)
-                 VALUES ('notice_feedback', 'Новый отзыв');
