9bddd1c5844de21c54b80a5a16521415f9ad14ca
allomir <alomir@yandex.ru>
2021-03-22 20:39:29 +0300

    Чистка

--- common/fixtures/TaskFilesFixture.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace common\fixtures;
-
-use yii\test\ActiveFixture;
-
-class TaskFilesFixture extends ActiveFixture
-{
-    public $modelClass = 'frontend\models\db\TaskFiles';
-}
--- common/fixtures/templates/task_files.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /**
-     * @var $faker \Faker\Generator
-     * @var $index integer
-     */
-    return [
-        'task_id' => rand(1, 10),
-        'file' => $faker->lexify('file_?????.???')
-    ];
